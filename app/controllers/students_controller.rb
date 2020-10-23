9d09da9d65770ca20ad0d9aea21602212bd4c913
kak79 <kohelka@gmail.com>
2020-10-23 11:29:39 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,29 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    find_student
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to student_path(@student)
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def find_student
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
 end
